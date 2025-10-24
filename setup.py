bbac31c8d2ebab0c31216a899b1d2ad88a23e4e5
Bekhruz Rakhmonov <bekhruzrakhmonov1@gmail.com>
2025-10-24 22:13:39 +0500

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='email_signature_detector',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
